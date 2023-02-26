4699190b5dbd4cad96cd70f391efa2b570b82d58
oldzhai <zfq0620@126.com>
2023-02-26 20:38:01 +0800

    Site updated: 2023-02-26 20:37:22

--- js/foot.js
+++ js/foot.js
@@ -1,4 +1,4 @@
-​
+<link rel="stylesheet" class="aplayer-secondary-style-marker" href="\assets\css\APlayer.min.css"><script src="\assets\js\APlayer.min.js" class="aplayer-secondary-script-marker"></script>​
 $(document).ready(function(e){
     show_date_time();
 })
